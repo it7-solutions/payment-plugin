b15ec421b3b47a9af7158ade20112960ddeb3c06
vherever <vherever@gmail.com>
2016-09-27 23:58:00 +0300

    data-binding to collect the data from the form, clear form after submit

--- app/components/selection-form.component.ts
+++ app/components/selection-form.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {Select} from "../models/select";
 import {PluginConfig} from "../services/plugin.config";
+import {SelectType} from "../models/select-type";
 
 @Component({
     selector: 'selection-form',
@@ -12,6 +13,12 @@ export class SelectionForm {
     online_systems: Select[];
     show_form: boolean;
 
+    info: SelectType = {
+        reg_service: '',
+        payment_type: '',
+        online_system: '',
+    };
+
     constructor(
         private _config: PluginConfig
     ) {
@@ -19,6 +26,22 @@ export class SelectionForm {
         this.payment_types = _config.payment_types;
         this.online_systems = _config.online_systems;
         this.show_form = _config.show_form;
+    }
+
+    public getInvoiceCall() {
+        console.log('info', this.info);
+
+        this.fillForm(
+            {
+                reg_service: '',
+                payment_type: '',
+                online_system: ''
+            }
+        );
+    }
+
+    fillForm(o: any) {
+        Object.assign(this.info, o);
     }
 
 }
---
+++ app/models/select-type.ts
@@ -0,0 +1,5 @@
+export interface SelectType {
+    reg_service: string;
+    payment_type: string;
+    online_system: string;
+}
